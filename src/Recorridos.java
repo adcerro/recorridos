257ef01ef513f62f96135eaee10f0fcef5bac43c
adcerro <adcerro@uninorte.edu.co>
2022-03-31 16:39:17 -0500

    feature: matrix multiplication and exponentiation

--- src/Recorridos.java
+++ src/Recorridos.java
@@ -4,10 +4,11 @@ public class Recorridos {
 
 
     public static void main(String[] args) {
-        int[][] adyMat = new int[25][25];
+
         Scanner leer = new Scanner(System.in);
         System.out.println("Digite dimensión n de la matriz de adyacencia:");
         int n = leer.nextInt();
+        int[][] adyMat = new int[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 int val = 0;
@@ -19,14 +20,40 @@ public class Recorridos {
             }
         }
         System.out.println("Matriz ingresada:");
-        for (int a = 0; a< n; a++) {
+        for (int a = 0; a < n; a++) {
             for (int b = 0; b < n; b++) {
                 System.out.print(adyMat[a][b]);
             }
             System.out.println("");
         }
-        System.out.println("Digite el vertice Vi desde el que desea empezar");
-        System.out.println("Digite el vertice Vj desde el que desea empezar");
+        System.out.println("Digite la longitud k de caminos de que desea hallar");
+        int k = leer.nextInt();
+        int[][] paths;
+        paths = matrixEponentiation(adyMat, k, n);
+        for (int posi = 0; posi < n; posi++) {
+            for (int posj = 0; posj < n; posj++) {
+                if(paths[posi][posj]!=0)
+                    System.out.println("Caminos de longitud " + k + " entre " + posi + " y " + posj + ": " + paths[posi][posj]);
+            }
+        }
+    }
 
+    public static int[][] matrixEponentiation(int[][] matA, int power, int size) {
+        int[][] result = matA;
+        for (int a = 1; a < power; a++) {
+            result = matrixMultilication(result, matA, size);
+        }
+        return result;
+    }
+
+    public static int[][] matrixMultilication(int[][] matA, int[][] matB, int size) {
+        int[][] result = new int[25][25];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                for (int b= 0; b<size;b++){
+                result[i][j] = result[i][j] + matA[i][b] * matB[b][j];}
+            }
+        }
+        return result;
     }
 }
